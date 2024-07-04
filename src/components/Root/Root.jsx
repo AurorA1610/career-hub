91efeed3aee94a889af2e5ed1a73c9bbe430f782
Aurora1610 <auroralroy2003@gmail.com>
2024-07-05 04:03:38 +0600

    scroll on top added

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import ScrollToTop from "../ScrollToTop/ScrollToTop";
 
 const Root = () => {
   return (
     <div data-theme="light">
+      <ScrollToTop></ScrollToTop>
       <div className="max-w-6xl mx-auto">
         <Header></Header>
         <Outlet></Outlet>
